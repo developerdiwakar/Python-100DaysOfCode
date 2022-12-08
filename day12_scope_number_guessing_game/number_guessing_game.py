e9c1fe68af8714aec6d9eebf7408688b504bbabd
Diwakar Kumar <developer.diwakar@gmail.com>
2022-12-08 17:35:29 +0530

    code refactoring

--- day12_scope_number_guessing_game/number_guessing_game.py
+++ day12_scope_number_guessing_game/number_guessing_game.py
@@ -1,12 +1,3 @@
-#Number Guessing Game Objectives:
-
-# Include an ASCII art logo.
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 from random import randint
 from art import logo
 
@@ -20,7 +11,6 @@ def getTotalAttempts():
     else:
         return HARD_LEVEL_ATTEMPTS
 
-
 def playGame():
     print(logo)
     
@@ -31,12 +21,11 @@ def playGame():
     # Set Answer
     answer = randint(1, 100)
     game_finished = False
-    
+
     while not game_finished:
         print(f"You have {remaining_attempts} left to guess the number.")
         guess = int(input("Make a guess: "))
-        if 100 < guess < 1:
-            continue
+        
         # apply game logic 
         if guess != answer:
             remaining_attempts -= 1
